01b42775519f4aba79e221ddc301dd9be109c0e6
c0l0l0s <hub4realtil@gmail.com>
2021-07-21 04:00:25 +0000

    more ui screens update

---
+++ Frontend/lib/year_selection.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'availablePrograms.dart';
+
+class yearSelection extends StatefulWidget {
+  const yearSelection({Key? key}) : super(key: key);
+
+  @override
+  _yearSelectionState createState() => _yearSelectionState();
+}
+
+class _yearSelectionState extends State<yearSelection> {
+
+  String name_of_program = 'computer';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: OrientationBuilder(
+          builder: (context, orientation)=>
+          orientation==Orientation.portrait?portrait_grid(name_of_program):landscape_listTiles(name_of_program),
+        ),
+      )
+    );
+  }
+}
+
+class portrait_grid extends StatelessWidget {
+  String name_of_program;
+
+  portrait_grid(this.name_of_program);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+        SizedBox(
+            height: MediaQuery.of(context).size.height*0.15
+        ),
+        Expanded(
+            child: GridView.builder(
+                itemCount: programYearCourses['${this.name_of_program}']['years'],
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 5.0,
+                    mainAxisSpacing: 10.0
+                ),
+                itemBuilder: (BuildContext context, int index){
+                  return Card(
+                    shadowColor: Color.fromRGBO(255, 100, 255, 1.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(200, 100, 255, 0.25)
+                      ),
+                      child: ListTile(
+                        onTap: (){
+                          print('portrait year ${index + 1} pressed');
+                        },
+                        title: Text('Year ${index + 1}'),
+                      ),
+                    ),
+                  );
+                })
+        )
+      ],
+    );
+  }
+}
+
+class landscape_listTiles extends StatelessWidget {
+  //String name_of_program = 'computer';
+  String name_of_program;
+
+  landscape_listTiles(this.name_of_program);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+              itemCount: programYearCourses['${this.name_of_program}']['years'],
+              itemBuilder: (BuildContext context, int index){
+                return Card(
+                  margin: EdgeInsets.all(12.0),
+                  shadowColor: Color.fromRGBO(255, 100, 255, 1.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(200, 100, 255, 0.25)
+                    ),
+                    child: ListTile(
+                      onTap: (){
+                        print('landScape year ${index +1} pressed');
+                      },
+                      title: Text('Year ${index + 1}'),
+                    ),
+                  ),
+                );
+              }),
+        )
+      ],
+    );
+  }
+}
+
+
+
+
+
