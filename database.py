adba6d8f25354a4c129e3ee68d5fe84df5aa1f74
Agbodjive Etorna k <akietorna@gmail.com>
2021-09-14 19:18:08 +0000

    update

--- database.py
+++ database.py
@@ -9,10 +9,10 @@ import MySQLdb.cursors
 #app.config['SECRET_KEY'] = 'hispresence123@'
 
 def connection():
-    connect = MySQLdb.connect(host="localhost",
-                              user="root",
-                              passwd="hispresence123@",
-                              db="library")
+    connect = MySQLdb.connect(host="slydhub.mysql.pythonanywhere-services.com",
+                              user="slydhub",
+                              passwd="@blossoms1",
+                              db="slydhub$library")
 # innitializing the cursor
     curs = connect.cursor()
 
